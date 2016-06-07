13ca14a6008b89fefff3496b8b1278f67cb49399
ikelab <bhha@pusan.ac.kr>
2016-06-07 20:13:27 +0900

    Update Complex1.java

--- ds2016/Complex1.java
+++ ds2016/Complex1.java
@@ -1,2 +1,15 @@
-1 2 3
-4 5 6
+package ds;
+
+public class Complex1 {
+	
+	public static void main(String[] args) {
+		double a1 = 3, b1 = 2;
+		double a2 = 1, b2 = 1;
+		double a3 = 2, b3 = 4;
+		
+		double a4 = a1 + a2, b4 = b1 + b2;
+		double a5 = a1 + a3, b5 = b1 + b3;
+		
+		System.out.println("Result: " + a4 + " + " + b4 + "i, " + a5 + " + " + b5 + "i");
+	}
+}
